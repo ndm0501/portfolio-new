1e7fe2663b72eb28a9d7a09384b5d773a6825b82
Mohammad Nadeem <mohammadnadeem@ITs-MacBook-Pro.local>
2021-06-23 22:38:16 +0530

    onclick props

--- src/components/common/ToggleSwitch.jsx
+++ src/components/common/ToggleSwitch.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './ToggleSwitch.css';
 
-const ToggleSwitch = () => {
+const ToggleSwitch = ({ onClick }) => {
   return (
-    <span className="toggle-switch">
+    <span className="toggle-switch" onClick={onClick}>
       <label className="switch">
         <input type="checkbox" />
         <span className="slider round"></span>
       </label>
     </span>
   );
+};
+
+ToggleSwitch.propTypes = {
+  onClick: PropTypes.func.isRequired
 };
 export default ToggleSwitch;
